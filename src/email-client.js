555073e898d1042d6027f931d91c17244de40df8
Midhun A Darvin <midhunadarvin@gmail.com>
2017-09-03 23:53:18 +0530

    change in nodemailer

--- src/email-client.js
+++ src/email-client.js
@@ -4,10 +4,17 @@ import Q from 'q';
 export default (emailObj) => {
 	var deferred = Q.defer();
 	var transporter = nodemailer.createTransport({
-		service: 'gmail',
+		// service: 'gmail',
+		host: 'smtp.gmail.com',
+		port: 465,
+		secure: true,
 		auth: {
 			user: 'cabotdeveloper@gmail.com',
 			pass: 'cabot1234'
+		},
+		tls: {
+			// do not fail on invalid certs
+			rejectUnauthorized: false
 		}
 	});
 
