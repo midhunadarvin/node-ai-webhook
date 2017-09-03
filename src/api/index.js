526bd09ef36e18f7eaed671a5cf7c23dbdb8385b
Midhun A Darvin <midhunadarvin@gmail.com>
2017-09-03 19:35:32 +0530

    add route

--- src/api/index.js
+++ src/api/index.js
@@ -3,13 +3,13 @@ import { Router } from 'express';
 import facets from './facets';
 import intelligence from './intelligence'
 
-export default ({ config, db, bot }) => {
+export default ({ config, db }) => {
 	let api = Router();
 
 	// mount the facets resource
 	api.use('/facets', facets({ config, db }));
 
-	api.use('/ai', intelligence({ config, bot }))
+	api.use('/ai', intelligence({ config }));
 
 	// perhaps expose some API metadata at the root
 	api.get('/', (req, res) => {
--- src/api/intelligence.js
+++ src/api/intelligence.js
@@ -3,8 +3,12 @@ import uniqid from 'uniqid';
 import ai from '../ai';
 import hangouts from '../hangouts';
 
-export default ({ config, bot }) => resource({
+export default ({ config }) => resource({
 
+	/** GET / - List all entities */
+	index({ body }, res) {
+		res.json(body);
+	},
 	/** POST / - Default entity */
 	create({ body }, res) {
 		
