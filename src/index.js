7086507a6af6a5eeb0782eaeadbec82963e216ad
Midhun A Darvin <midhunadarvin@gmail.com>
2017-09-03 19:11:46 +0530

    remove hangouts bot

--- src/index.js
+++ src/index.js
@@ -27,55 +27,21 @@ app.use(bodyParser.json({
 }));
 // connect to db
 // initializeDb(db => {
-hangouts().then((bot) => {
-	const agent = ai({ config });
+const agent = ai({ config });
 
-	bot.on('online', () => {
-		console.log('online');
+// internal middleware
+app.use(middleware({ config }));
 
-		// internal middleware
-		app.use(middleware({ config }));
+// api router
+app.use('/api', api({ config }));
 
-		// api router
-		app.use('/api', api({ config, bot }));
-
-		app.server.listen(process.env.PORT || config.port, () => {
-			console.log(`Started on port ${app.server.address().port}`);
-		});
-	});
-
-	bot.on('message', (from, message) => {
-		console.log(from + ">> " + message);
-
-		var request = agent.textRequest(message, {
-			sessionId: uniqid(config)
-		});
-
-		request.on('response', function (response) {
-			// console.log(response);
-			bot.sendMessage(from, response.result.fulfillment.speech);
-			// res.json({
-			// 	'result': response.result.fulfillment.speech
-			// });
-		});
-
-		request.on('error', function (error) {
-			console.log(error);
-		});
-
-		request.end();
-
-		// switch (message) {
-		// 	case "help":
-		// 		bot.sendMessage(from, "I an example Hangouts bot. Try saying hello.");
-		// 		break;
-		// 	case "hello":
-		// 		bot.sendMessage(from, "Why hello to you too.");
-		// 		break;
-		// }
-	});
-
+app.server.listen(process.env.PORT || config.port, () => {
+	console.log(`Started on port ${app.server.address().port}`);
 });
+
+// var request = agent.textRequest(message, {
+// 	sessionId: uniqid(config)
+// });
 // });
 
 
